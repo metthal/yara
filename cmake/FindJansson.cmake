47bf75dcaf6219b8d4feec85c7eff770433f5b9d
Marek Milkovič <marek.milkovic@avast.com>
2017-12-17 17:42:51 +0100

    Added finders for libmagic and jansson, added build of modules

---
+++ cmake/FindJansson.cmake
@@ -0,0 +1,45 @@
+# Finder for Jansson library.
+#
+# Imported targets:
+# Jansson
+#
+# Result variables:
+# JANSSON_FOUND - System has found Jansson library.
+# JANSSON_INCLUDE_DIR - Jansson include directory.
+# JANSSON_LIBRARIES - Jansson library files.
+# JANSSON_VERSION - Version of Jansson library.
+
+if(UNIX)
+    find_package(PkgConfig QUIET)
+    pkg_check_modules(PKG_JANSSON QUIET jansson)
+endif()
+
+find_path(JANSSON_INCLUDE_DIR
+    NAMES jansson.h
+    HINTS ${PKG_JANSSON_INCLUDEDIR})
+
+find_library(JANSSON_LIBRARIES
+    NAMES jansson
+    HINTS ${PKG_JANSSON_LIBDIR})
+
+mark_as_advanced(JANSSON_INCLUDE_DIR JANSSON_LIBRARIES)
+
+if(JANSSON_INCLUDE_DIR AND JANSSON_LIBRARIES)
+    set(JANSSON_FOUND 1)
+
+    file(STRINGS "${JANSSON_INCLUDE_DIR}/jansson.h" JANSSON_VERSION_DEFINE REGEX "^#[ \t]*define[ \t]+JANSSON_VERSION[ \t]+\"[^\"]+\"")
+    string(REGEX REPLACE "^#[ \t]*define[ \t]+JANSSON_VERSION[ \t]+\"([^\"]+)\"" "\\1" JANSSON_VERSION "${JANSSON_VERSION_DEFINE}")
+
+    if(NOT TARGET Jansson)
+        add_library(Jansson UNKNOWN IMPORTED)
+        set_target_properties(Jansson PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${JANSSON_INCLUDE_DIR}")
+        set_target_properties(Jansson PROPERTIES IMPORTED_LOCATION "${JANSSON_LIBRARIES}")
+    endif()
+endif()
+
+find_package_handle_standard_args(Jansson
+    REQUIRED_VARS
+        JANSSON_LIBRARIES
+        JANSSON_INCLUDE_DIR
+    VERSION_VAR
+        JANSSON_VERSION)
