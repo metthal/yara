49db3437b0dae9d06354014cbe57db6109900878
Marek Milkovič <milkovic.marek@gmail.com>
2019-03-03 21:53:47 +0100

    Correct directory for tests

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p build
+rm -rf build && mkdir build
 cd build
 cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCUCKOO_MODULE=ON -DMAGIC_MODULE=ON -DYARA_TESTS=ON ..
 cmake --build . -- -j
