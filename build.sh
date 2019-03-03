a2750a7c9d0af8bd8290311703b57996466c0ecf
Marek Milkovič <milkovic.marek@gmail.com>
2019-03-03 16:09:35 +0100

    Try to specify OpenSSL root dir

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 cd build
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DYARA_TESTS=ON ..
+cmake -G"Unix Makefiles" -DOPENSSL_ROOT_DIR=c:/cygwin64 -DCMAKE_BUILD_TYPE=Release -DYARA_TESTS=ON ..
 cmake --build . -- -j
